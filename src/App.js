3e4d96625d12f798c436b9459ab8ed27b73743de
rockthepeople <3qufqks@gmail.com>
2024-05-27 19:42:58 +0900

    upload : vendingmachine

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
+// app.js
+import React from 'react';
+import ReactDOM from 'react-dom';
 import './App.css';
+import { keypair_2, infuraKey, employee_test } from './account';
+import { EthrDID } from 'ethr-did';
+import { useEffect, useState } from 'react';
+import { Resolver } from 'did-resolver';
+import { getResolver } from 'ethr-did-resolver';
 
-function App() {
+const App = ({ result }) => {
+
+  const providerConfig = { name: 'mainnet', rpcUrl: `https://mainnet.infura.io/v3/${infuraKey}` };
+  const ethrDidResolver = getResolver(providerConfig);
+  const resolver = new Resolver(ethrDidResolver);
+
+  const [recoverdDID, setRecoveredDID] = useState("")
+  const ethrDid_2 = new EthrDID({ ...keypair_2 });
+  const didRecover = async () => {
+    const did = localStorage.getItem("planzDID");
+    if (!did) {
+      return false;
+    }
+    const res = await ethrDid_2.verifyJWT(did, resolver);
+    console.log([res]);
+    setRecoveredDID([res]);
+  }
+
+  const [checkSigner, setCheckSigner] = useState(false);
+  const vendingMachineCheck = async () => {
+    const res = await didRecover();
+    if (!res) {
+      alert('검증할 DID가 없습니다');
+      return;
+    };
+    const companyAccount = '0x9021361C5226099AA99370DfeD181c9E31469d3B'
+    if (recoverdDID) {
+      const signer = recoverdDID[0].signer.blockchainAccountId;
+      const tokens = signer.split(':');
+      const lastToken = tokens[tokens.length - 1];
+      console.log("signer: " + lastToken);
+      if (companyAccount === lastToken) {
+        setCheckSigner(true);
+        alert("유효한 서명 값, 커피 제조가 시작됩니다 !")
+      }
+      return;
+    }
+    return;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="app">
+      <div className="inner-box">
+        <h1>{result}</h1>
+        <h3>DID 정보 풀어서 체크 (커피머신) <button onClick={vendingMachineCheck} style={{ marginTop: "30px" }}>verify</button></h3>
+        {recoverdDID.length ?
+          <div>
+            <p>요청자 : {recoverdDID[0].payload.claims.account}</p>
+            <p>서명자 : {recoverdDID[0].signer.blockchainAccountId}</p>
+          </div> : <></>
+        }
+        {checkSigner ? <p>유효한 서명값입니다. 커피 제조 시작! </p> : <></>}
+      </div>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/account.js
@@ -0,0 +1,34 @@
+export const keypair_1 = {
+    address: '0x9021361C5226099AA99370DfeD181c9E31469d3B',
+    privateKey: '0x1de50334b47bc59027ecb6450637c333a57566941e29e51859c030de1261662b',
+    publicKey: '0x0361e0984afc0e6dbb76a2fc2f7f1e86d63613f42ce032a8e26a812c82bd9bc1e3',
+    identifier: '0x0361e0984afc0e6dbb76a2fc2f7f1e86d63613f42ce032a8e26a812c82bd9bc1e3'
+}
+
+export const keypair_2 = {
+    address: '0xedf57bC0e2775433E97d85c996bB03e1dbaa57c5',
+    privateKey: '0xf0606ffc7b56978633d766bf6d4ab3ddbd441ae83d2bb85110017287960aeb2a',
+    publicKey: '0x024b6eb1a2be6962813f27864c22bc0a283ae5fffa099a7cea7eceef0b088eebf6',
+    identifier: '0x024b6eb1a2be6962813f27864c22bc0a283ae5fffa099a7cea7eceef0b088eebf6'
+}
+
+export const keypair_3 = {
+    privateKey : '0x55dab1939f33363cc4527eeb1bfb5371ed632a0b9257ead6aa2b7bb63003e0da'
+}
+
+export const test_key = {
+    address : '0x9E56a749EDa10bE733FF8f13b365DaEA2357448D',
+    privateKey : '0xdfd12e059955617255681d70ed2724f51d637bdd51581ffb88c2279fa2289461',
+    publicKey: '0x026990c19798ad04bd62a06b786202a9688e489aaa227985c3ebdbc2848f30e035',
+    identifier: '0x026990c19798ad04bd62a06b786202a9688e489aaa227985c3ebdbc2848f30e035',
+}
+
+export const employee_test = {
+    address : '0x9E56a749EDa10bE733FF8f13b365DaEA2357448D',
+    privateKey : '0xdfd12e059955617255681d70ed2724f51d637bdd51581ffb88c2279fa2289461',
+    publicKey: '0x14E6eCBb6C60F1D403295B44c8C1F7cf23F59452',
+    identifier: '0x026990c19798ad04bd62a06b786202a9688e489aaa227985c3ebdbc2848f30e035',
+}
+
+export const infuraKey = "c249e2789fff4a6db1fd3ea782ffe5a4"
+
